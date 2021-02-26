ae9369e88605c89ec71c51a3bad6b72adcf70c0e
Kr_Knev <kneav@students.softuni.bg>
2021-02-26 13:36:03 +0200

    Update app.js

--- Asynchronous Programing and Promise/02.Github-Repos/scripts/app.js
+++ Asynchronous Programing and Promise/02.Github-Repos/scripts/app.js
@@ -1,3 +1,24 @@
 function loadRepos() {
-	console.log("TODO...");
+    let username = document.querySelector('#username').value;
+    const url = `https://api.github.com/users/${username}/repos`;
+    let result = document.querySelector('#repos');
+
+    function _createLi(_url, _text) {
+        let li = document.createElement('li');
+        result.appendChild(li);
+        let a = document.createElement('a');
+        li.appendChild(a);
+        a.href = _url;
+        a.textContent = _text;
+    }
+    fetch(url)
+        .then(promise => promise.json())
+        .then(data => {
+            result.innerHTML = '';
+            data.forEach(element => {
+                _createLi(element.svn_url, element.full_name)
+                console.log((element))
+            });
+        });
+
 }
