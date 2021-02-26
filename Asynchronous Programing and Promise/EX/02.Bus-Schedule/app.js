89459260f4f76b3813a4f4bea7f264aad79ddf37
Kr_Knev <kneav@students.softuni.bg>
2021-02-27 00:10:46 +0200

    HomeWork
    EX_2

--- Asynchronous Programing and Promise/EX/02.Bus-Schedule/app.js
+++ Asynchronous Programing and Promise/EX/02.Bus-Schedule/app.js
@@ -1,11 +1,39 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    let btnDepart = document.querySelector('#depart');
+    let btnArive = document.querySelector('#arrive');
+    const infoLable = document.querySelector('#info span');
+
+
+    const url = `http://localhost:3030/jsonstore/bus/schedule/`;
+
+    let currentStop = {
+        next: 'depot'
+    };
+
+    // async function _makerequest(_id) {
+    //     const promise = await fetch(url + _id);
+    //     currentStop = await promise.json();
+    // }
+
+    async function depart() {
+
+        //  _makerequest(currentStop.next);
+        const promise = await fetch(url + currentStop.next);
+        currentStop = await promise.json();
+
+        infoLable.textContent = `Next stop ${currentStop.name}`;
+        btnDepart.disabled = true;
+        btnArive.disabled = false;
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        infoLable.textContent = `Arriving at ${currentStop.name}`;
+
+
+        btnDepart.disabled = false;
+        btnArive.disabled = true;
     }
 
     return {
