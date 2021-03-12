5b66b8fc80fb1ee95d44d4c958f76fa4998cda2f
Kr_Knev <kneav@students.softuni.bg>
2021-03-12 11:12:44 +0200

    homework

---
+++ 06 Client Side Rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/01.List-Towns/app.js
@@ -0,0 +1,17 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit() {
+    event.preventDefault();
+    let formData = new FormData(form);
+    let result = Array.from(formData.entries())
+        .reduce((p, [k, v]) => Object.assign({
+            [k]: v
+        }), {})
+    render(html `<ul></ul>`, document.querySelector('#root'));
+    render(result['towns'].split(', ')
+        .map((town) => html `<li>${town}</li>`), document.querySelector('#root > ul'));
+}
