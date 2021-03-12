1806a0f6037f78517b8cb3571c479df7e451c7dd
Kr_Knev <kneav@students.softuni.bg>
2021-03-12 17:39:14 +0200

    Update dropdown.js

--- 06 Client Side Rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
+++ 06 Client Side Rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,49 @@
-function addItem() {
-    console.log('TODO:...');
+import { json } from '../client/api.js';
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+let form = document.querySelector('form');
+
+async function _loadOptions() {
+    let res = (await json(url));
+    //   console.log(res);
+    render(Object.keys(res).map(key => html `
+        <option value=${res[key]._id}>${res[key].text}</option>
+        `), document.querySelector('#menu'));
+
+}
+
+render(html `
+<label for="itemText">
+Text:
+</label>
+<input type="text" id="itemText" />
+<label for="itemText">
+Value:
+</label>
+<input type="text" id="itemValue" />
+<input type="submit" value="Add">
+`, document.querySelector('form'));
+
+_loadOptions();
+
+document.querySelector('form').addEventListener('submit', addItem);
+async function addItem() {
+    event.preventDefault();
+    console.log(document.querySelector('form'))
+        //   let formData = new FormData(document.querySelector('form'));
+
+    let result = await json(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            text: document.querySelector('#itemText').value
+        })
+    });
+    if (!result.hasOwnProperty('_id')) {
+        return alert('result.status');
+    }
+    _loadOptions();
 }
