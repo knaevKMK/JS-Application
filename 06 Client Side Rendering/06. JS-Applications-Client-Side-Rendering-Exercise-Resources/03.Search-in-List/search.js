e3e912d2ba0364a6b91f47f67d48571a33bf1dc0
Kr_Knev <kneav@students.softuni.bg>
2021-03-12 13:42:22 +0200

    Update search.js

--- 06 Client Side Rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ 06 Client Side Rendering/06. JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -1,3 +1,33 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+render(html `<ul></ul>`, document.querySelector('#towns'));
+
+
+render(towns
+    .map(town => html `
+<li class="null">${town}</li>
+`), document.querySelector('#towns > ul'));
+
+let input = document.querySelector('article > input');
+document.querySelector('article > button').addEventListener('click', search);
+
 function search() {
-   // TODO
+    let regex = input.value.trim();
+
+    if (regex === '') {
+        return alert('The input field is Empty')
+    }
+    let matches = 0;
+    Array.from(document.querySelector('#towns > ul').children)
+        .forEach(town => {
+            if (town.textContent.toLowerCase().includes(regex.toLowerCase())) {
+                matches++;
+                town.className = 'active';
+            } else {
+                town.className = 'null';
+            }
+        });
+    render(html `<p>${matches == 0 ? 'No' : matches} ${matches == 1 ? 'match' : 'matches'} found</p > `, document.querySelector('#result'));
+
+    input.value = '';
 }
