b71a26b66929c8b6c794b4408bd3ef6ad81b3e04
Kr_Knev <kneav@students.softuni.bg>
2021-04-08 18:32:25 +0300

    refacture

--- 09 CANVAS/src/app.js
+++ 09 CANVAS/src/app.js
@@ -4,31 +4,43 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
+window.addEventListener('resize', function() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
 const c = canvas.getContext('2d');
-const colors = ['red', 'blue', 'green', 'orange', 'yellow', 'black'];
+const colors = ['red', 'pink', 'purple', 'blue', 'green', 'orange', 'yellow', 'black'];
 let figureArray = [];
-const pcs = 20;
 
 
+const pcs = 50;
 for (let index = 0; index < pcs; index++) {
-    let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
-    let dx = (Math.random() * 0.5) * 60;
-    let dy = (Math.random() * 0.5) * 40;
-    let color = colors[Math.round(Math.random() * colors.length)];
     const r = 155;
+    let x = Math.random() * (window.innerWidth - r * 2) + r;
+    let y = Math.random() * (window.innerHeight - r * 2) + r;
+    let dx = (Math.random() * -0.5) * 10;
+    let dy = (Math.random() * 0.5) * 10;
+    let color = colors[Math.round(Math.random() * colors.length)];
     figureArray.push(new Circle(x, y, dx, dy, r, color, c))
 
 }
 
 
 function animate() {
-    requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
+    _handleCircles()
+    requestAnimationFrame(animate);
+
+}
+animate();
+
+function _handleCircles() {
 
     for (let index = 0; index < figureArray.length; index++) {
         figureArray[index].update();
+        if (figureArray[index].r < 0.6) {
+            figureArray.splice(index, 1);
+            index--;
+        }
     }
 }
-animate();
